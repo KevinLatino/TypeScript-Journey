3e1301136c55de48dca542cb76d206ef79468c36
Kevin Latino <111583322+KevinLatino@users.noreply.github.com>
2024-09-06 12:24:42 -0600

    Adding tuples

---
+++ src/Advance/Tuples.ts
@@ -0,0 +1,9 @@
+let persona: [string, number, boolean];
+
+persona = ["kevin", 12, true];
+
+const [personName, age, check] = persona;
+
+console.log(personName);
+console.log(age);
+console.log(check);
