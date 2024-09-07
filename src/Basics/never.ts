d54952afe7b4e533d75cdc3574a506af073f310c
Kevin Latino Marin <KevinLatino>
2024-09-07 17:29:34 -0600

    never type

---
+++ src/Basics/never.ts
@@ -0,0 +1,11 @@
+(() => {
+
+    //we use "never" type when we have a function that will never stop
+
+    const neverExample = (): never => {
+        while(true){
+            console.log("never stop learning");
+        }
+    }
+
+})();
