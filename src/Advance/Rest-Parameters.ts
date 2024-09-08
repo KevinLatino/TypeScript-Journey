280b96f00950ef10371a4c57cac30b9976033f0d
Kevin Latino Marin <KevinLatino>
2024-09-07 19:52:11 -0600

    rest-params

---
+++ src/Advance/Rest-Parameters.ts
@@ -0,0 +1,17 @@
+(() => {
+
+    enum Roles  {
+        Admin = "Admin",
+        client = "client"
+    }
+
+    const checkRol = (...roles: string[]) => {
+        if (roles.includes(Roles.Admin)){
+            return true
+        }
+        return false
+    }
+
+    console.log(checkRol(Roles.client));
+    
+})();
