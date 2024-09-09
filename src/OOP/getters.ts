82993dc138e66805710fc09d0f1b6ebb925319f3
KevinLatino <latinokevin9@gmail.com>
2024-09-08 19:41:57 -0600

    getters

---
+++ src/OOP/getters.ts
@@ -0,0 +1,15 @@
+class Dates {
+    constructor(
+        private _day: number = 10,
+        public mounth: number = 10,
+        private year: number = 10
+    ) { }
+
+    get day(): number {
+        return this._day
+    }
+}
+
+const date = new Dates();
+
+console.log(date.day);
